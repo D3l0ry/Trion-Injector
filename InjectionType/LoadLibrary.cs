5a269d0cabd58355208089be8c8fd83212f2890f
Даниил <D3l0ry@outlook.com>
2020-08-22 16:35:17 +0500

    Optimization code

---
+++ InjectionType/LoadLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.MemoryInteraction;
+using System.Text;
+
+namespace Trion_Injector.InjectionType
+{
+    internal class LoadLibrary:IInjector
+    {
+        public ReturnCode Injecting(string dllName, string path, string exportName, Process process, MemoryManager memoryManager)
+        {
+            bool export = string.IsNullOrWhiteSpace(exportName);
+            Executor executor = memoryManager.GetExecutor();
+
+            if (!File.Exists(path)) return ReturnCode.FILE_NOT_FOUND;
+
+            byte[] pathBytes = Encoding.Unicode.GetBytes(path);
+
+            if (!executor.CallFunction(Executor.GetFunction("kernel32.dll", "LoadLibraryW"), pathBytes)) return ReturnCode.INJECTING_ERROR;
+
+            if (!export)
+            {
+                ReturnCode exportCode = InjectHelper.CallExportFunction(dllName, exportName, process, executor);
+
+                if (exportCode == ReturnCode.EXPORT_FUNCTION_ERROR || exportCode == ReturnCode.CALL_FUNCTION_ERROR) return exportCode;
+            }
+
+            return ReturnCode.INJECTION_SUCCESSFUL;
+        }
+    }
+}
