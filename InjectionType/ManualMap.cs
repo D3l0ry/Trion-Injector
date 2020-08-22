9e4585a47e80f2cd026eda35b124d8ef27aecd14
Даниил <D3l0ry@outlook.com>
2020-08-22 16:35:23 +0500

    Updated

---
+++ InjectionType/ManualMap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.MemoryInteraction;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Diagnostics.ProcessExtensions;
+
+namespace Trion_Injector.InjectionType
+{
+    class ManualMap : IInjector
+    {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate IntPtr fnLoadLibrary(string dllName);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate IntPtr fnGetProcAddress(IntPtr hModule, string procName);
+
+        private struct Loader
+        {
+            public fnGetProcAddress pGetProcAddress;
+            public fnLoadLibrary pLoadLibrary;
+        }
+
+        private void ShellCode(ref Loader p)
+        {
+            fnLoadLibrary pLoadLibrary = p.pLoadLibrary;
+            fnGetProcAddress pGetProcAddress = p.pGetProcAddress;
+        }
+
+        public ReturnCode Injecting(string dllName, string path, string exportName, Process process, MemoryManager memoryManager)
+        {
+            return ReturnCode.INJECTION_SUCCESSFUL;
+        }
+    }
+}
